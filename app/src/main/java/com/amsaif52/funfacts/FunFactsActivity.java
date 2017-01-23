b81f1d2efb3799943e29fc4805e410bf447a08cd
amsaif52 <am52saif@gmail.com>
2017-01-22 22:02:29 -0500

    added functionality

--- app/src/main/java/com/amsaif52/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/amsaif52/funfacts/FunFactsActivity.java
@@ -2,12 +2,33 @@ package com.amsaif52.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+//                String fact = "Ostriches can run faster than horses.";
+                String fact = mFactBook.getFactBook();
+                mFactTextView.setText(fact);
+            }
+        };
+
+        mShowFactButton.setOnClickListener(listener);
     }
 }
